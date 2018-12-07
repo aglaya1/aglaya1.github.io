036c731a778a5d595bb0b3a584aa107c686bc037
epamekids <40541820+epamekids@users.noreply.github.com>
2018-12-07 22:08:51 +0300

    start

---
+++ card_1/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import './card.html';
+import App from './App';
+import Card from './Card';
+import Nim from './Nim';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <div>
+    <Nim count = "100"></Nim>
+  </div>
+  , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
